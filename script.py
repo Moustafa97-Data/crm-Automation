3c1f8b1c0fab312e48e1afc6b5feb8f366cd0253
Moustafa97-Data <mostafa.smir97@gmail.com>
2026-04-01 13:58:29 +0200

    Update script.py

--- script.py
+++ script.py
@@ -12,7 +12,6 @@ def run_script():
         "https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/drive"
     ]
-
     
     creds_dict = json.loads(os.environ['GOOGLE_CREDS'])
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
